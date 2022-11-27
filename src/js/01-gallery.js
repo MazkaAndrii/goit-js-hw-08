224f07a8b0dad0b5dad303c51f76ab458d462e4a
Mazka Andrii <andriy.mazka@gmail.com>
2022-11-27 14:52:16 +0200

    part1fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,41 +8,14 @@ console.log(galleryItems);
 const galleryContainer = document.querySelector('.gallery');
 const galleryItemsMarkUp = galleryItems
   .map(item => {
-    return `<div class="gallery__item">
-   <a class="gallery__link" href="${item.original}">
-      <img
-         class="gallery__image"
-         src="${item.preview}"
-         data-source="${item.original}"
-         alt="${item.description}"
-      />
-   </a>
-</div>`;
+    return `<a class="gallery__item" href="${item.original}">
+   <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`;
   })
   .join('');
 
 galleryContainer.insertAdjacentHTML('afterbegin', galleryItemsMarkUp);
-galleryContainer.addEventListener('click', onClick);
-
-function onClick(e) {
-  e.preventDefault();
-  if (e.target.nodeName !== 'IMG') {
-    return;
-  }
-  const instance = basicLightbox.create(
-    `<img src=${e.target.dataset.source} />`,
-    {
-      onShow: () => {
-        document.addEventListener('keydown', onEscape);
-      },
-      onClose: () => {
-        document.removeEventListener('keydown', onEscape);
-      },
-    }
-  );
-  instance.show();
-
-  function onEscape(e) {
-    if (e.key === 'Escape') instance.close();
-  }
-}
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
